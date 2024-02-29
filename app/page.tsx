0f606f1525145c0aacf96a1f5fa793e39c1f7f7a
XyphrusCaguan <caguanxyphrus@gmail.com>
2024-02-29 23:34:42 +0800

    Update: Comments on codes

--- app/page.tsx
+++ app/page.tsx
@@ -21,19 +21,11 @@ import {
 import {
   Card,
   CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
 } from "@/components/ui/card"
-
-
 
 import { GetItems, postItemData, updateItemData } from "../services/services";
 
-import { collection, addDoc, onSnapshot, updateDoc, doc, deleteDoc, query, where, getDocs } from "firebase/firestore";
-import { ref, onValue } from "firebase/database";
-import { JSXElementConstructor, PromiseLikeOfReactNode, ReactElement, ReactNode, ReactPortal, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
 
@@ -63,32 +55,36 @@ export default function Home() {
   const [ postalert, setPostAlert] = useState(false);
   const [ updatealert, setUpdateAlert] = useState(false);
 
+  // handle for showing additional inputs
+  // on options, price, cost, stock 
   const handleAddVariety = async (e: any) => {
     e.preventDefault();
     setShow(true);
   }
 
+  // handle for closing additional inputs
+  // on options, price, cost, stock 
   const handleCloseVariety = async (e: any) => {
     e.preventDefault();
     setShow(false);
   }
 
-  // console.log(GetItems());
-  let products = GetItems();
-  const uId = Object.keys(products).length+1;
 
+  let products = GetItems();
+  const uId = Object.keys(products).length+1; // declaration for Unique ID when adding items
+
+  // handle for adding items to the database
   const handleSubmit = async (e: any) => {
     e.preventDefault();
     const arrayOption = [option, option2, option3]
     const arrayPrice = [price, price2, price3]
     const arrayCost = [cost, cost2, cost3]
     const arrayStock = [stock, stock2, stock3]
-    setPostAlert(true)
+    setPostAlert(true) // alert will show after adding items
 
     const timePost = setTimeout(() => {
-      // After 3 seconds set the show value to false
       setPostAlert(false)
-    }, 3000)
+    }, 3000) // alert will close in 3 seconds
 
     postItemData(uId, name, category , arrayOption , arrayPrice , arrayCost , arrayStock);
     return () => {
@@ -96,6 +92,7 @@ export default function Home() {
     }
   }
 
+  // handle for updating items on the database
   const handleUpdate = async (e: any) => {
     e.preventDefault();
     
@@ -103,33 +100,23 @@ export default function Home() {
     const arrayPrice = [price, price2, price3]
     const arrayCost = [cost, cost2, cost3]
     const arrayStock = [stock, stock2, stock3]
-    setUpdateAlert(true)
+    setUpdateAlert(true) // alert will show after updating items
 
     const timeUpdate = setTimeout(() => {
-      // After 3 seconds set the show value to false
       setUpdateAlert(false)
-    }, 3000)
+    }, 3000) // alert will close in 3 seconds
 
     updateItemData(uuid, name, category , arrayOption , arrayPrice , arrayCost , arrayStock);
   }
 
-  useEffect(() => {
-    // when the component is mounted, the alert is displayed for 3 seconds
-    setTimeout(() => {
-      setPostAlert(false);
-    }, 3000);
-
-    setTimeout(() => {
-      setUpdateAlert(false);
-    }, 3000);
-  }, []); 
-
   return (
     <>
     <main className="min-h-screen min-w-128 p-2">
+      {/* alert for post */}
       {postalert && <div className={`alert alert-${postalert ? 'absolute ': 'hidden'} fixed bg-green-400 w-1/4 h-16 p-4 z-99 inset-x-0 mx-auto text-center`}>
         <span className="text-black text-2xl">Data added!</span>  
       </div>}
+      {/* aert for update */}
       {updatealert && <div className={`alert alert-${updatealert ? 'absolute' : 'hidden'} fixed bg-green-400 w-1/4 h-16 p-4 z-99 inset-x-0 mx-auto text-center`}>
         <span className="text-black text-2xl">Data updated!</span>  
       </div>}
@@ -160,13 +147,13 @@ export default function Home() {
           </div>
           <div>
           <Dialog>
-            <DialogTrigger className="flex items-center bg-green-500 px-2 py-1 rounded text-white text-xl">
+            <DialogTrigger className="flex items-center bg-green-500 px-2 py-1 rounded text-white text-xl"> {/* Button will trigger a pop-up */}
             <svg viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg" className="w-8">
             <path d="M13 6C13 5.44771 12.5523 5 12 5C11.4477 5 11 5.44771 11 6V11H6C5.44771 11 5 11.4477 5 12C5 12.5523 5.44771 13 6 13H11V18C11 18.5523 11.4477 19 12 19C12.5523 19 13 18.5523 13 18V13H18C18.5523 13 19 12.5523 19 12C19 11.4477 18.5523 11 18 11H13V6Z" fill="currentColor"/>
             </svg>
               Add Product
             </DialogTrigger>
-            <DialogContent className="max-w-screen-md ">
+            <DialogContent className="max-w-screen-md "> {/* Content of the pop-up for adding items */}
               <DialogHeader>
                 <DialogTitle>Add Product</DialogTitle>
               </DialogHeader>
@@ -193,7 +180,7 @@ export default function Home() {
                   </div>
                 </div>
                 <div className="flex flex-col">
-                  {/* option --> stock inputs2 */}
+                  {/* option --> stock initial inputs */}
                 <div className="flex gap-2 pb-2 pr-10">
                   <Input 
                     type="text" 
@@ -224,7 +211,7 @@ export default function Home() {
                   />
                 </div>
                 <div className={`${show ? 'block' : 'hidden'} w-full h-full flex justify-center items-center gap-2 pb-2`}>
-                  {/* option --> stock inputs2 */}
+                  {/* option --> stock inputs2, when there are 2 variety/option */}
                   <Input 
                     type="text" 
                     placeholder="Options (small, medium, large etc.)" 
@@ -253,13 +240,13 @@ export default function Home() {
                     onChange={(e) => setStock2(e.target.value)}
                   />
                 <div className="flex items-center justify-center">
-                  <button className="text-red-600 w-8 h-8" onClick={handleCloseVariety}>
+                  <button className="text-red-600 w-8 h-8" onClick={handleCloseVariety}> {/* button/icon will close additional inputs */}
                   <svg fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M5.755,20.283,4,8H20L18.245,20.283A2,2,0,0,1,16.265,22H7.735A2,2,0,0,1,5.755,20.283ZM21,4H16V3a1,1,0,0,0-1-1H9A1,1,0,0,0,8,3V4H3A1,1,0,0,0,3,6H21a1,1,0,0,0,0-2Z"/></svg>
                   </button>
                 </div>
                 </div>
                 <div className={`${show ? 'block' : 'hidden'} w-full h-full flex justify-center items-center gap-2 pb-2`}>
-                  {/* option --> stock inputs3 */}
+                  {/* option --> stock inputs3, when there are 2 variety/option */}
                   <Input 
                     type="text" 
                     placeholder="Options (small, medium, large etc.)" 
@@ -288,7 +275,7 @@ export default function Home() {
                     onChange={(e) => setStock3(e.target.value)}
                   />
                 <div className="flex items-center justify-center">
-                  <button className="text-red-600 w-8 h-8" onClick={handleCloseVariety}>
+                  <button className="text-red-600 w-8 h-8" onClick={handleCloseVariety}> {/* button/icon will close additional inputs */}
                   <svg fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M5.755,20.283,4,8H20L18.245,20.283A2,2,0,0,1,16.265,22H7.735A2,2,0,0,1,5.755,20.283ZM21,4H16V3a1,1,0,0,0-1-1H9A1,1,0,0,0,8,3V4H3A1,1,0,0,0,3,6H21a1,1,0,0,0,0-2Z"/></svg>
                   </button>
                 </div>
@@ -297,10 +284,10 @@ export default function Home() {
                   <Button 
                     variant="outline" 
                     className="w-full text-green-500 border-green-500 border-2 border-spacing-8 border-dashed"
-                    onClick={handleAddVariety}
+                    onClick={handleAddVariety} 
                   >
                     Add Variety
-                  </Button>
+                  </Button> {/* button will show additional inputs */}
                 </div>
                 <div className="flex justify-center">
                   <Button 
@@ -310,7 +297,7 @@ export default function Home() {
                     onClick={handleSubmit}
                   >
                     Add Item
-                  </Button>
+                  </Button> {/* button for submitting inputs */}
                 </div>
               </div>
               </form>
@@ -322,9 +309,10 @@ export default function Home() {
         
         {/* Product Card */}
         <div className="h-full gap-16 p-12 grid grid-cols-3">
+          {/* Mapping data from database  */}
           {GetItems().map((item, index) => {
 
-            
+            // initialization of array options, price, cost, stock
             const arrayOption: any[] = [];
             const arrayPrice: any[] = [];
             const arrayCost: any[] = [];
@@ -350,13 +338,13 @@ export default function Home() {
                   <div className="flex justify-center w-full">
                     {/* carousel options --> stocks */}
                     
-                    <Carousel className="w-3/4 h-full">
+                    <Carousel className="w-3/4 h-full"> 
                       <CarouselContent>
                         {Array.from({ length: 3 }).map((_, index) => (
-                          <CarouselItem key={index}>
+                          <CarouselItem key={index}> {/* map the contents of each array */}
                             <div className="p-1">
                               <Card>
-                                <CardContent className="flex aspect-square justify-center p-6 flex-col gap-2">
+                                <CardContent className="flex aspect-square justify-center p-6 flex-col gap-2"> {/* display data of option --> stock */}
                                   {arrayOption.map((arrayoption)=>{
                                     return(
                                       <div key={index}>
@@ -398,10 +386,10 @@ export default function Home() {
                 </div>
                 <div className="flex pt-5">
                 <Dialog>
-                  <DialogTrigger className="flex items-center bg-gray-500 px-2 py-1 rounded text-white text-xl">
+                  <DialogTrigger className="flex items-center bg-gray-500 px-2 py-1 rounded text-white text-xl"> {/* Button will trigger pop-up for updating items */}
                     Edit
                   </DialogTrigger>
-                    <DialogContent className="max-w-screen-md " key={index}>
+                    <DialogContent className="max-w-screen-md " key={index}> {/* Content of items will also appear as placeholder on updates */}
                     <DialogHeader>
                       <DialogTitle>Update Product</DialogTitle>
                     </DialogHeader>
@@ -554,7 +542,7 @@ export default function Home() {
                           onClick={handleUpdate}
                         >
                           Update Item
-                        </Button>
+                        </Button> {/* Button to update data to the database */}
                       </div>
                     </div>
                     </form>
